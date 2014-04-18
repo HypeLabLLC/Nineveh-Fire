4d162a9d9df237f0ea237a6b3a202c85e8ec7558
Jordan <skorlir@gmail.com>
2014-04-18 16:40:31 -0500

    Adds a bunch of stuff and instantiates a barebones calendar -- HIDE the calendar if you can't update before meeting

--- js/main.js
+++ js/main.js
@@ -2,4 +2,21 @@
 $("#menu-toggle").click(function(e) {
     e.preventDefault();
     $("#wrapper").toggleClass("active");
+    $('#page-content-overlay').fadeToggle();
+});
+
+function verticalize() {
+    $('v-center').height(function() {
+        return $(this).prev().height();
+    });
+}
+
+$(window).on('resize', function() {
+    verticalize();
+});
+
+$(window).on('load', function() {
+   verticalize(); 
+    $('#sidebar-wrapper a').smoothScroll();
+    $('.calendarSpace').clndr();
 });
