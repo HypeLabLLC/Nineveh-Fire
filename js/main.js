5829ba7ee3a78e46dc351f51f1f1afd942b58714
skorlir <skorlir@gmail.com>
2014-06-19 15:20:34 -0400

    updates before launch ready; pushed already to ninevehfire.org

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 //Custom JavaScript for the Menu Toggle 
-$("#menu-toggle").click(function(e) {
+$("#menu-toggle, #page-content-overlay").click(function(e) {
     e.preventDefault();
-    $("#wrapper").toggleClass("active");
+    $("#wrapper, html").toggleClass("active");
+    $('#page-content-overlay, .emergency-icons').toggleClass("active");
     $('#page-content-overlay').fadeToggle();
 });
 
 function verticalize() {
-    $('v-center').height(function() {
+    $('.v-center').height(function() {
         return $(this).prev().height();
     });
 }
@@ -16,12 +17,18 @@ $(window).on('resize', function() {
     $('.headshot>img').height(function() {
       return $(this).width();
     });
+    if ($('.sidebar-contact').position().top < 500)
+      $('.sidebar-contact').css({position: 'static'});
 });
 
 
 window.onload = function() {
-   verticalize(); 
-    $('.headshot>img').width('200px');
+   verticalize();
+    $('.headshot>img').width('200px').height(function() {
+      return $(this).width();
+    });
+    if ($('.sidebar-contact').position().top < 500)
+      $('.sidebar-contact').css({position: 'static'});
     $('.calendarSpace').clndr();
     $('a').smoothScroll();
 };
